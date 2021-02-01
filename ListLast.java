c6be7bc7f1dc7ca151dcd9f21e11e2dfbeb7b113
Julian Matuszewski <600018222@fjuhsd.org>
2021-02-01 17:39:47 +0000

    added doesRepeatLast method. Added test data to Main

--- ListLast.java
+++ ListLast.java
@@ -1,15 +1,24 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Julian Matuszewski
+//Date - 2/1/21
 
 import java.util.List;
 import java.util.ArrayList;
 
 public class ListLast
 {
-	public static boolean go( List<Integer> ray)
+	public static boolean doesRepeatLast( List<Integer> list)
 	{
-		return true;
+    //set new int equal to last element
+    int last = list.get(list.size()-1);
+    //for loop moving through but stopping before last index
+    for (int i = 0; i <= list.size()-2; i++)
+      //if statement checks if each number equals last
+      if (list.get(i) == last) 
+        //if passed, return true
+        return true;
+    //return false if no successful checks
+    return false;
 	}
 }
--- Main.java
+++ Main.java
@@ -1,13 +1,20 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Julian Matuszewski
+//Date - 2/1/21
 
 //add imports as needed
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main
 {
  public static void main( String args[] )
  {
+    System.out.println( ListLast.doesRepeatLast( Arrays.asList(21,32,43,124,5)));//false
+    System.out.println( ListLast.doesRepeatLast( Arrays.asList(12,1342,54,7,-1,12)));//true
+    System.out.println( ListLast.doesRepeatLast( Arrays.asList(213,231,1231,213,213)));//true
+    System.out.println( ListLast.doesRepeatLast( Arrays.asList(-999,213,1023,900,32,56)));//false
  }
 }
